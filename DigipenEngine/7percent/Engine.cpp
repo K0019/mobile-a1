c6522a13a99a59d183230ada02c6f0337cdc696d
K0019 <39440047+K0019@users.noreply.github.com>
2025-09-04 10:48:36 +0800

    Restore C# Scripting

--- DigipenEngine/7percent/Engine.cpp
+++ DigipenEngine/7percent/Engine.cpp
@@ -39,6 +39,8 @@ All rights reserved.
 
 #include "ryan-c/Renderer.h"
 #include "ryan-c/VulkanHelper.h"
+#include "CSScripting.h"
+#include "HotReloader.h"
 
 #include "fa.h"
 
@@ -193,6 +195,11 @@ void Engine::OnFocusChanged([[maybe_unused]] GLFWwindow* window, int isFocused)
 {
 	CONSOLE_LOG(LEVEL_DEBUG) << "Focused changed: " << isFocused;
 
+#ifdef IMGUI_ENABLED
+	// TODO: Clean this call up
+	HotReloader::FocusCallBackReload(window, isFocused);
+#endif
+
 	Messaging::BroadcastAll("OnWindowFocus", static_cast<bool>(isFocused));
 }
 void Engine::onFullscreen()
@@ -281,6 +288,9 @@ void Engine::init()
 	ST<GameSettings>::Get()->Load(); // Only load settings from file first so we have the correct filepaths.
 
 	ST<Console>::Get()->SetupCrashHandler(); // DO NOT REMOVE THIS LINE EVER
+
+	// Scripting Engine Initialisation
+	CSharpScripts::CSScripting::Init();
 
 	// FMOD Initialisation
 	ST<AudioManager>::Get()->Initialise();
@@ -627,6 +637,9 @@ void Engine::run() {
 
 		// update game state
 		// -----------------
+#ifdef IMGUI_ENABLED
+		CSharpScripts::CSScripting::CheckCompileUserAssemblyAsyncCompletion();
+#endif
 		ST<Game>::Get()->Update();
 		ST<Scheduler>::Get()->Update(GameTime::FixedDt() * static_cast<float>(GameTime::NumFixedFrames()));
 
@@ -711,6 +724,8 @@ void Engine::shutdown() {
 #ifdef IMGUI_ENABLED
 	ST<Hierarchy>::Destroy();
 #endif
+
+	CSharpScripts::CSScripting::Exit();
 
 	ecs::Shutdown();
 
---
+++ DigipenEngine/Assets/Scripts/TestScript.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using EngineScripting;
+
+public class TestScript : EID
+{
+	TestScript()
+	{
+	}
+
+    // This method is called once when the script is first initialized
+    public void OnCreate()
+    {
+       
+    }
+
+    // This method is called once per frame
+    void OnUpdate(float dt)
+    {
+        Debug.Log("Hello");
+    }
+}
