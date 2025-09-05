9847848872c8797a8d528b4606d386778fc251e2
K0019 <39440047+K0019@users.noreply.github.com>
2025-09-05 20:50:49 +0800

    Update TestScript.cs

--- DigipenEngine/Assets/Scripts/TestScript.cs
+++ DigipenEngine/Assets/Scripts/TestScript.cs
@@ -21,14 +21,15 @@ public class TestScript : ComponentBase
     // This method is called once on the frame when the script is initialized
     void Start()
     {
-
+        Debug.Log("TestScript started with value " + value.ToString());
     }
 
     // This method is called once per frame
     void Update(float dt)
     {
+        Transform transform = this.transform;
+        transform.position = transform.position + dt;
+
         value += 1;
-        Debug.Log(gameObject.GetComponent<TestScript>().value.ToString());
-        
     }
 }
