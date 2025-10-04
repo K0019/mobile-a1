086438882556b39af2225e63aa69bc4a6b1771ea
Mr Rock <44529454+FeelsFeels@users.noreply.github.com>
2025-10-04 08:57:11 +0800

    wait i gotta redo this part

--- engine/Engine/Resources/MaterialSerialization.cpp
+++ engine/Engine/Resources/MaterialSerialization.cpp
@@ -39,51 +39,6 @@ void MaterialSerialization::Serialize(Serializer& writer, const ProcessedMateria
 
 void MaterialSerialization::Deserialize(Deserializer& reader, ProcessedMaterial& outMaterial)
 {
-    reader.DeserializeVar("name", &outMaterial.name);
-
-    std::string alphaModeStr;
-    if (reader.DeserializeVar("alphaMode", &alphaModeStr))
-    {
-        if (alphaModeStr == "Mask") outMaterial.alphaMode = AlphaMode::Mask;
-        else if (alphaModeStr == "Blend") outMaterial.alphaMode = AlphaMode::Blend;
-        else outMaterial.alphaMode = AlphaMode::Opaque;
-    }
-
-    reader.DeserializeVar("alphaCutoff", &outMaterial.alphaCutoff);
-    reader.DeserializeVar("metallicFactor", &outMaterial.metallicFactor);
-    reader.DeserializeVar("roughnessFactor", &outMaterial.roughnessFactor);
-    reader.DeserializeVar("normalScale", &outMaterial.normalScale);
-    reader.DeserializeVar("occlusionStrength", &outMaterial.occlusionStrength);
 
 
-    std::array<float, 4> baseColorArray;
-    if (reader.DeserializeVar("baseColorFactor", &baseColorArray))
-    {
-        outMaterial.baseColorFactor = { baseColorArray[0], baseColorArray[1], baseColorArray[2], baseColorArray[3] };
-    }
-
-    std::array<float, 3> emissiveArray;
-    if (reader.DeserializeVar("emissiveFactor", &emissiveArray))
-    {
-        outMaterial.emissiveFactor = { emissiveArray[0], emissiveArray[1], emissiveArray[2] };
-    }
-
-    std::unordered_map<std::string, std::string> texturePaths;
-    if (reader.DeserializeVar("textures", &texturePaths))
-    {
-        if (texturePaths.contains("baseColor") && !texturePaths["baseColor"].empty())
-            outMaterial.baseColorTexture.source = FilePathSource{ texturePaths["baseColor"] };
-
-        if (texturePaths.contains("normal") && !texturePaths["normal"].empty())
-            outMaterial.normalTexture.source = FilePathSource{ texturePaths["normal"] };
-
-        if (texturePaths.contains("metallicRoughness") && !texturePaths["metallicRoughness"].empty())
-            outMaterial.metallicRoughnessTexture.source = FilePathSource{ texturePaths["metallicRoughness"] };
-
-        if (texturePaths.contains("emissive") && !texturePaths["emissive"].empty())
-            outMaterial.emissiveTexture.source = FilePathSource{ texturePaths["emissive"] };
-
-        if (texturePaths.contains("occlusion") && !texturePaths["occlusion"].empty())
-            outMaterial.occlusionTexture.source = FilePathSource{ texturePaths["occlusion"] };
-    }
 }
