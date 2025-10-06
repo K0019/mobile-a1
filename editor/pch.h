6c59c14b01821db662ce9017f3ffba39bab63cbb
K0019 <39440047+K0019@users.noreply.github.com>
2025-10-06 15:40:51 +0800

    Editor can use MagicEngine

---
+++ editor/pch.h
@@ -0,0 +1,24 @@
+﻿/******************************************************************************/
+/*!
+\file   pch.h
+\par    Project: 7percent
+\par    Course: CSD2401
+\par    Section B
+\par    Software Engineering Project 4
+\date   02/05/2025
+
+\author Kendrick Sim Hean Guan (100%)
+\par    email: kendrickheanguan.s\@digipen.edu
+\par    DigiPen login: kendrickheanguan.s
+
+\brief
+  This is a pre-compiled header file to speed up compilation.
+
+All content © 2024 DigiPen Institute of Technology Singapore.
+All rights reserved.
+*/
+/******************************************************************************/
+
+#pragma once
+
+#include "../MagicEngine-core/pch.h"
