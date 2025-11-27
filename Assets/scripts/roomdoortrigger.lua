0d3b83f4468bc6ab010b760552210afda08f6a1e
mbrine <42174429+mbrine@users.noreply.github.com>
2025-11-28 02:26:26 +0800

    Added temp collider to second room

--- Assets/scripts/roomdoortrigger.lua
+++ Assets/scripts/roomdoortrigger.lua
@@ -1,5 +1,7 @@
 local thisEntity
 local opensDoor = false
+local enemySpawnDelay = 3.0
+local spawnIncoming = false
 function start(entity)
     local nameComp = entity:GetNameComponent()
 
@@ -20,11 +22,11 @@ function OnTriggerEnter(entity)
     local nameComp = entity:GetNameComponent();
     if nameComp:Exists() then
         if(nameComp.name == "Player") then
-            Magic.Log(Magic.LogLevel.info, nameComp.name)
+            --Magic.Log(Magic.LogLevel.info, nameComp.name)
             local entityContainer = thisEntity:GetEntityReferenceHolderComponent()
 
             if entityContainer:Exists() then
-                Magic.Log(Magic.LogLevel.info, "Yay?")
+                --Magic.Log(Magic.LogLevel.info, "Yay?")
                 local doorEntity = entityContainer:GetEntityReference(0)
                 local scriptComp = doorEntity:GetScriptComponent()
 
@@ -33,16 +35,34 @@ function OnTriggerEnter(entity)
                 else
                     scriptComp:CallScriptFunction("close")
                 end
-            -- Call relevant functions on enemy wave spawner etc here!
-                --local spawnerEntity = entityContainer:GetEntityReference(1)
-                --local spawnerScriptComp = spawnerEntity:GetScriptComponent()
 
-                --if spawnerScriptComp:Exists() then
-                --Magic.Log(Magic.LogLevel.info, "It's spawnin time")
-                --spawnerScriptComp:CallScriptFunction("wavespawn")
-                --end
+                spawnIncoming = true;
             end
         end
     end
 
+end
+
+function update(entity)
+    if spawnIncoming then
+        if enemySpawnDelay <= 0.0 then
+            local entityContainer = entity:GetEntityReferenceHolderComponent()
+
+            if entityContainer:Exists() then
+            -- Call relevant functions on enemy wave spawner etc here!
+                if entityContainer:GetSize() >= 2 then
+                    local spawnerEntity = entityContainer:GetEntityReference(1)
+                    local spawnerScriptComp = spawnerEntity:GetScriptComponent()
+
+                    if spawnerScriptComp:Exists() then
+                        --Magic.Log(Magic.LogLevel.info, "It's spawnin time")
+                        spawnerScriptComp:CallScriptFunction("wavespawn")
+                    end
+                end
+            end
+
+            entity:Destroy()
+        end
+        enemySpawnDelay = enemySpawnDelay - Magic:DeltaTime()
+    end
 end
