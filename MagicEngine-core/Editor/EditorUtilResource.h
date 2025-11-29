422fff7470337ab75a990767026cde2a3eb481c2
rycallibrx <110918162+rycallibrx@users.noreply.github.com>
2025-11-30 03:25:18 +0800

    Update EditorUtilResource.h

--- MagicEngine-core/Editor/EditorUtilResource.h
+++ MagicEngine-core/Editor/EditorUtilResource.h
@@ -9,7 +9,10 @@ namespace editor {
 	template <typename ResourceType>
 	void EditorUtil_DrawResourceHandle(const char* label, UserResourceHandle<ResourceType>& resourceHandle)
 	{
-		static_assert(false, "EditorUtil_DrawResourceHandle not implemented for this type");
+		static_assert(always_false<ResourceType>::value, "EditorUtil_DrawResourceHandle not implemented for this type");
+	  // note, always_false is defined in properties.h, it's not an actual cpp type
+	  // the bug comes from different versions of the compiler evaluating static_assert at different times
+	  // previously it would be evaluated at compile time, even without a function instantiation. Putting a template forces it to only be evaluated when instantiated.
 	}
 
 	template <> void EditorUtil_DrawResourceHandle(const char* label, UserResourceHandle<ResourceTexture>& resourceHandle);
