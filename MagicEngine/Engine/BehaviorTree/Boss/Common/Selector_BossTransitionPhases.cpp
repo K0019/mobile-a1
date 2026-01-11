a30d29276e6d8501ffd250c312285c4523cff175
mbrine <42174429+mbrine@users.noreply.github.com>
2026-01-11 21:30:50 +0800

    Boss transition time between phases

--- MagicEngine/Engine/BehaviorTree/Boss/Common/Selector_BossTransitionPhases.cpp
+++ MagicEngine/Engine/BehaviorTree/Boss/Common/Selector_BossTransitionPhases.cpp
@@ -13,22 +13,33 @@ void S_Boss_TransitionPhases::OnInitialize()
 
 NODE_STATUS S_Boss_TransitionPhases::OnUpdate([[maybe_unused]] ecs::EntityHandle entity)
 {
-    if (entity)
-    {
-        while (currentChildItr != childrenPtr.end())
-        {
-            //Run the child node.
-            NODE_STATUS stat{ (*currentChildItr)->Tick(entity) };
+    // Sanic Tea
+    if (!entity)
+        return NODE_STATUS::FAILURE;
 
-            //If the child returns failure or running, return the same thing.
-            if (stat != NODE_STATUS::SUCCESS)
-                return stat;
-
-            //If the child succeeded, go to the next node.
-            ++currentChildItr;
-        }
-        //If all the node succeeded, return success.
+    // Once we reach the end of the list, return success
+    if (currentChildItr == childrenPtr.end())
         return NODE_STATUS::SUCCESS;
+
+    // If in transition preiod, we just return RUNNING and decrease transition time
+    if (currentTransitionDelay > 0.0f)
+    {
+        currentTransitionDelay -= GameTime::Dt();
+        return NODE_STATUS::RUNNING;
     }
-    return NODE_STATUS::FAILURE;
+
+    // Tick current child
+    NODE_STATUS stat = (*currentChildItr)->Tick(entity);
+
+    // If not success, just return it
+    if (stat != NODE_STATUS::SUCCESS)
+    {
+        return stat;
+    }
+
+    // Child succeeded, start transition delay and ++ current child
+    ++currentChildItr;
+    currentTransitionDelay = transitionDelay;
+
+    return NODE_STATUS::RUNNING;
 }
