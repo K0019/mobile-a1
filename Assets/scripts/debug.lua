0fc88394d5f707c39ad87a31c9cef31bf1912532
DireVision <50040768+DireVision@users.noreply.github.com>
2026-02-23 11:03:21 +0800

    debug

--- Assets/scripts/debug.lua
+++ Assets/scripts/debug.lua
@@ -1,1 +1,13 @@
-Magic.Log(Magic.LogLevel.error, "HELLO")
+-- Simple debug script that logs every update frame
+
+function start(entity)
+    Magic.Log(Magic.LogLevel.info, "[DEBUG] Script started")
+end
+
+function update(entity, dt)
+    Magic.Log(Magic.LogLevel.info, "[DEBUG] Update called, dt = " .. tostring(dt))
+end
+
+function onDestroy(entity)
+    Magic.Log(Magic.LogLevel.info, "[DEBUG] Script destroyed")
+end
